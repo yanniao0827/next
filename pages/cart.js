d400fc8af9e0c19b0964e29da5bec22481b76335
yanniao0827 <yanning0113niao@gmail.com>
2024-06-19 14:34:26 +0800

    刪除購物車商品

--- pages/cart.js
+++ pages/cart.js
@@ -5,7 +5,7 @@ export default function Cart() {
     const cartKey = "shin-cart";
     const [cart, setCart] = useState([]);
 
-    useEffect(() => {
+    const getCartFromStorage = () => {
         const oriData = localStorage.getItem(cartKey);
         let cartData = []; // 預設值
         try {
@@ -14,10 +14,21 @@ export default function Cart() {
             cartData = [];
           }
         } catch (ex) {}
+        return cartData;
+      };
     
-        setCart(cartData);
+    
+  // 移除項目
+  const cartRemoveItem = (pid) => {
+    let cartData = getCartFromStorage();
+    const resultCart = cartData.filter((p) => p.id !== pid);
+    localStorage.setItem(cartKey, JSON.stringify(resultCart));
+    setCart(resultCart);
+  };
+
+    useEffect(() => {
+        setCart(getCartFromStorage());
       }, []);
-    
   return (
     <Layout1>
       <h2>購物車</h2>
@@ -41,7 +52,7 @@ export default function Cart() {
                 <td>{p.price}</td>
                 <td>{p.quantity}</td>
                 <td>{p.quantity*p.price}</td>
-                <td><button type="button" class="btn btn-danger"  onClick={() =>{} }>刪除商品</button></td>
+                <td><button type="button" class="btn btn-danger"  onClick={() => cartRemoveItem(p.id)}>刪除商品</button></td>
             </tr>
         })
     }
