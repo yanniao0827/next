ab7a31229351f6af0cca5ec957a1ff1f50292c37
yanniao0827 <yanning0113niao@gmail.com>
2024-06-24 14:59:17 +0800

    clearEffect

--- components/common/life-a.js
+++ components/common/life-a.js
@@ -2,11 +2,18 @@ import React,{useEffect} from 'react'
 
 export default function LifeA() {
     useEffect(()=>{
+        let n = 0;
         console.log("已掛載");
+
+        const interval_id=setInterval(()=>{
+            n++;
+            console.log({n});
+        },1000)
         return()=>{
-            console.log("卸載前");
+            console.log("卸載");
+            clearInterval(interval_id);
         };
-    });
+    },[]);
 
 
     return <div>LifeA</div>;
