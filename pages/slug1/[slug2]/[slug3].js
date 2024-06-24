7d0652962438447b61b8d23f82f7a210d385fc6c
yanniao0827 <yanning0113niao@gmail.com>
2024-06-24 11:27:24 +0800

    router.isReady

--- pages/slug1/[slug2]/[slug3].js
+++ pages/slug1/[slug2]/[slug3].js
@@ -3,9 +3,14 @@ import React from 'react'
 
 export default function Slug3() {
     const router=useRouter();
+
+    console.log('router.isReady:',router.isReady);
+    console.log("router.query.slug3:",router.query.slug3);
   return (
     <pre>
       {JSON.stringify(router.query,null,4)}
+
+      
     </pre>
   )
 }
