ee6fc6efaf6039dc58b6e343e2e066b6125171f0
yanniao0827 <yanning0113niao@gmail.com>
2024-06-25 11:50:52 +0800

    可控表單

--- pages/ab-add.js
+++ pages/ab-add.js
@@ -5,61 +5,102 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function AbAdd() {
-    return <Layout1 title="新增通訊錄" pageName="ab-add">
-    <div className="row">
-      <div className="col-6">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">新增資料</h5>
-            <form
-              name="form1"
-            >
-              <div className="mb-3">
-                <label htmlFor="name" className="form-label">姓名</label>
-                <input type="text" className="form-control" id="name" name="name" />
-                <div className="form-text"></div>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email</label>
-                <input type="text" className="form-control" id="email" name="email" />
-                <div className="form-text"></div>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="mobile" className="form-label">手機</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="mobile"
-                  name="mobile"
-                />
-                <div className="form-text"></div>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="birthday" className="form-label">生日</label>
-                <input
-                  type="date"
-                  className="form-control"
-                  id="birthday"
-                  name="birthday"
-                />
-                <div className="form-text"></div>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="address" className="form-label">地址</label>
-                <textarea
-                  className="form-control"
-                  id="address"
-                  name="address"
-                  cols="30"
-                  rows="3"
-                ></textarea>
-                <div className="form-text"></div>
-              </div>
-              <button type="submit" className="btn btn-primary">新增</button>
-            </form>
+  const [myForm, setMyForm] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    birthday: "",
+    address: "",
+  });
+  return (
+    <Layout1 title="新增通訊錄" pageName="ab-add">
+      <div className="row">
+        <div className="col-6">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">新增資料</h5>
+              <form name="form1">
+                <div className="mb-3">
+                  <label htmlFor="name" className="form-label">
+                    姓名
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    value={myForm.name}
+                    onChange={(e) => {
+                      console.log(e.target.value);
+                      const newForm = { ...myForm, name: e.target.value };
+                      setMyForm(newForm);
+                    }}
+                  />
+                  <div className="form-text"></div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                  />
+                  <div className="form-text"></div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="mobile" className="form-label">
+                    手機
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="mobile"
+                    name="mobile"
+                  />
+                  <div className="form-text"></div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="birthday" className="form-label">
+                    生日
+                  </label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="birthday"
+                    name="birthday"
+                  />
+                  <div className="form-text"></div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="address" className="form-label">
+                    地址
+                  </label>
+
+                  <textarea
+                    className="form-control"
+                    id="address"
+                    name="address"
+                    cols="30"
+                    rows="3"
+                  ></textarea>
+                  <div className="form-text"></div>
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  新增
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    </Layout1>;
-  }
+    </Layout1>
+  );
+}
