5f3adf2f617ae6db9f9cfba2b199225f3742db4d
yanniao0827 <yanning0113niao@gmail.com>
2024-06-27 09:36:58 +0800

    準備做登入的jwt context

--- config/api-path.js
+++ config/api-path.js
@@ -7,3 +7,6 @@ export const AB_ITEM_DELETE = `${API_SERVER}/address-book/api`;
 export const AB_GET_ITEM = `${API_SERVER}/address-book/api`;
 
 export const AB_UPDATE_PUT = `${API_SERVER}/address-book/api`;
+
+
+export const JWT_LOGIN_POST = `${API_SERVER}/login-jwt`;
--- contexts/auth-context.js
+++ contexts/auth-context.js
@@ -1,8 +1,27 @@
 import { createContext,useContext} from "react";
 
 const AuthContext=createContext();
+const storageKey="Healthier-auth";
 
 export function AuthContextProvider({children}){
+    const login=async(email,password)=>{
+        try{
+            const r=await fetch(JWT_LOGIN_POST,{
+                method:"POST",
+                body:JSON.stringify({email,password}),
+                headers:{"Content-Type":"application/json",},
+            });
+            const result=await r.json();
+            if(result.success){
+                localStorage.setItem(storageKey,JSON.stringify(result.data));
+            }
+            return result.successl;
+        }catch(ex){
+            console.log(ex);
+            return false;
+        }
+    };
+
     return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
 }
 
